decc155fe254b011fcdf007539b0ee68e55618ef
¨Vinicius <viniciuscamillodev@gmail.com>
2026-05-11 14:34:51 -0300

    Saving

--- Exercicios Gemini/Exercicio31.java
+++ Exercicios Gemini/Exercicio31.java
@@ -1,3 +1,45 @@
+/*
+ * QUESTÃO 31: ARRAYLIST (A LISTA DINÂMICA)
+ * Escreva um programa que:
+ * 1. Importe: import java.util.ArrayList;
+ * 2. Declare: ArrayList<String> compras = new ArrayList<>();
+ * 3. Use um while(true) para pedir itens.
+ * 4. SE o usuário digitar "sair", use o comando "break" para sair do loop.
+ * 5. SENÃO, use compras.add(item) para adicionar na lista.
+ * 6. Ao final, use um for para exibir todos os itens e o total de itens (compras.size()).
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Exercicio31 {
-    
+    public static void main(String[] arg){
+    Scanner input = new Scanner(System.in);
+
+    ArrayList<String> compras = new ArrayList<>();
+    String produto;
+
+
+    while (true) {
+        System.out.println("Digite o produto (ou sair para finalizar)");
+        produto = input.nextLine();
+
+
+        if (produto.equalsIgnoreCase("Sair")) {
+            break;
+        }
+        compras.add(produto);
+    }
+
+    for(int i = 0; i < compras.size(); i++){
+        System.out.println("Item " + (i+1) +": " + compras.get(i));
+    }
+
+    System.out.println("Total de items: " + compras.size());
+
+    input.close();
+
+
+
+    }
 }
---
+++ Exercicios Gemini/Exercicio32.java
@@ -0,0 +1,51 @@
+/*
+ * QUESTÃO 32: MÉDIA COM ARRAYLIST
+ * Escreva um programa que:
+ * 1. Declare um ArrayList de números inteiros: 
+ * ArrayList<Integer> notas = new ArrayList<>();
+ * 2. Use um while(true) para pedir notas (números inteiros).
+ * 3. Se o usuário digitar um número negativo (ex: -1), o programa para de pedir.
+ * 4. Após o loop, use um FOR para somar todas as notas guardadas na lista.
+ * Dica: para pegar a nota use notas.get(i)
+ * 5. Calcule e exiba a média final.
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Exercicio32 {
+    public static void main(String[] arg){
+
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> notas = new ArrayList<>();
+        int nota ;
+        int total = 0;
+
+        while ( true ){
+            System.out.println("Insira as notas dos alunos(ou nota negativa para sair)");
+            nota = input.nextInt();
+
+            if ( nota < 0 ){
+                break;
+            }
+
+            notas.add(nota);
+        }
+
+        for (int i = 0; i < notas.size() ; i++){
+            total = total + notas.get(i);                
+        }
+        
+        if (notas.size() > 0 ){
+            double media = (double) total / notas.size();
+            System.out.println("A media total e: " + media);
+        } else{
+            System.out.println("Nenhuma nota foi inserida");
+        }
+
+        input.close();
+
+    }
+    
+}
