ae618fab254349ea5c0e301da70668ebcf40510a
¨Vinicius <viniciuscamillodev@gmail.com>
2026-05-13 18:05:17 -0300

    Strugling with arrayList

--- Exercicios Gemini/Exercicio33.java
+++ Exercicios Gemini/Exercicio33.java
@@ -1,3 +1,46 @@
+/*
+ * QUESTÃO 33: O BUSCADOR DE NOMES
+ * 1. Crie um ArrayList<String> chamado "convidados".
+ * 2. Peça para o usuário cadastrar 5 nomes (use um for simples de 0 a 5).
+ * 3. Depois, peça para o usuário digitar UM nome para pesquisar.
+ * 4. Use um novo FOR para percorrer a lista e um IF para verificar:
+ * - SE convidados.get(i).equalsIgnoreCase(nomePesquisado)
+ * - ENTÃO exiba: "Nome encontrado na posição " + i
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Exercicio33 {
+    public static void main(String[] arg){
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<String> convidados = new ArrayList<>();
+        String convidado;
+
+        for ( int i = 0 ; i < 6 ; i ++){
+            System.out.println("Cadastre os nomes do convidados");
+            convidado = input.nextLine();
+            convidados.add(convidado);
+        }
+
+        System.out.println("Qual nome voce deseja pesquisar");
+        String pesquisar = input.nextLine();
+
+
+        for ( int i = 0; i < convidados.size() ; i ++) {
+            if ( convidados.get(i).equalsIgnoreCase(pesquisar)) {
+                System.out.println("Nome encontrado na posicao" + i);
+
+            } 
+
+        }
+
+
+
+        input.close();
+
+        }
+
+    }
     
-}
